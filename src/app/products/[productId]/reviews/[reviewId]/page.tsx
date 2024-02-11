1cf96026eb69174e3af8f72e9b4246da392a4e3e
Rishikesh Mahale <rishikeshmahale16@gmail.com>
2024-02-11 23:56:29 +0530

    new chapters

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from "next/navigation";
+
 export default function ReviewDetail({ params }: {
     params: {
         productId: string,
         reviewId : string
     }
 }) {
+
+    if (parseInt(params.reviewId) > 1000) {
+        notFound();
+    }
     
     return (
         <>
